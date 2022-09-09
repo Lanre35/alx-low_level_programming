4df5caddfce4f532ace9eecfdb7256c2f64ab938
Lanre35 <lanreazeez99@yahoo.com>
2022-09-09 03:28:46 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  *
- * Description: "Programing is like building a multillingual puzzle"
+ * Description: "Programming is like building a multillingual puzzle"
  * Return: 0
  */
 int main(void)
 {
-	puts("\"Programing is like building a multillingual puzzle");
+	puts("\"Programming is like building a multillingual puzzle");
 	return (0);
 }

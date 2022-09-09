6aa86153994828bb3a0316e7d5c136327248ee7c
Lanre35 <lanreazeez99@yahoo.com>
2022-09-09 04:05:49 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: using the main function
+ * this program prints "programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer,but the outcome is a place of art,\n");
+	return (0);
+}

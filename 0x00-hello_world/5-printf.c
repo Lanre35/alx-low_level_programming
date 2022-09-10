aca0451d648ad2027d28d16aa1fed6c2f7795ea0
Lanre35 <lanreazeez99@yahoo.com>
2022-09-09 23:12:47 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * main - Entry point
  *
  * Descripion: using the main function
- * this program prints "Programming is like building a multilingual puzzle
+ * this program prints "Programming is like building a multilingual puzzle"
  * Return: 0
  */
 int main(void)

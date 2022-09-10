e911196156630fc3c49da8bbc3c40ba4f6feaba8
Lanre35 <lanreazeez99@yahoo.com>
2022-09-10 01:32:58 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: 1
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora korper, 2015-10-2019\n", 59);
+	return (1);
+}
+

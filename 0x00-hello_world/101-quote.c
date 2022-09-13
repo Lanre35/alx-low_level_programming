1af98cd3e64ba405f94e28b351d709b412e52a22
Lanre35 <lanreazeez99@yahoo.com>
2022-09-12 19:16:59 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,12 +6,12 @@
  *
  * Descripion: using the main function
  * and that of place and that of art is useful" - Dora karpar,2019"
- * Return: 0
+ * Return: 1
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
+	puts(STDOUT_FILENO, "and that piece of art is useful
 			\" - Dora korper, 2015-10-2019\n", 59);
-	return (0);
+	return (1);
 }
 

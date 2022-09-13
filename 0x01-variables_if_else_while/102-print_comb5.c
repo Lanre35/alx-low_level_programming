b8d36351d1898f6c3d0f6ef61486dadbc526d75e
Lanre35 <lanreazeez99@yahoo.com>
2022-09-12 17:47:32 -0700

    task_102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,16 +11,16 @@ int main(void)
 {
 	int n, m;
 
-	for (n = 0; n <= 99; n++)
+	for (n = 0; n <= 98; n++)
 	{
-		for (m = n + 1; m <= 99; m++)
+		for (m = n + 0; m <= 99; m++)
 		{
 			putchar((n / 10) + '0');
 			putchar((n % 10) + '0');
 			putchar(' ');
 			putchar((m / 10) + '0');
 			putchar((m % 10) + '0');
-			if (n == 99 && m == 99)
+			if (n == 98 && m == 99)
 				continue;
 			putchar(',');
 			putchar(' ');

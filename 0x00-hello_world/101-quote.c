5da4e1c39a28b975efd4fb3e958551a7776ec86a
Lanre35 <lanreazeez99@yahoo.com>
2022-09-12 19:31:35 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
+	fwrite(STDOUT_FILENO, "and that piece of art is useful
 			\" - Dora korper, 2015-10-2019\n", 59);
 	return (1);
 }

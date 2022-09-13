126dfd9295752386edf3590badfb200e46279edc
Lanre35 <lanreazeez99@yahoo.com>
2022-09-12 17:15:20 -0700

    task_101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,18 +11,18 @@ int main(void)
 {
 	int n1, n2, n3;
 
-	for (n1 = 0; n1 <= 8; n1++)
+	for (n1 = '0'; n1 <= '9'; n1++)
 	{
-		for (n2 = n1 + 1; n2 <= 9; n2++)
+		for (n2 = n1 + 1; n2 <= '9'; n2++)
 		{
-			for (n3 = n2 + 1; n3 <= 9; n3++)
+			for (n3 = n2 + 1; n3 <= '9'; n3++)
 			{
 				if ((n2 != n1) != n3)
 				{
-					putchar(n1 % 10) + '0');
-					putchar(n2 % 10) + '0');
-					putchar(n3 % 10) + '0');
-					if (n1 == 7 && n2 == 8)
+					putchar(n1);
+					putchar(n2);
+					putchar(n3);
+					if (n1 == '7' && n2 == '8')
 						continue;
 					putchar(',');
 					putchar(' ');

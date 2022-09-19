ea011d7e44526d1f2ae8f9a19f203abd05bd8878
Lanre35 <lanreazeez99@yahoo.com>
2022-09-19 10:56:32 -0700

    done

--- 0x05-pointers_arrays_strings/4-main.c
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -1,4 +1,4 @@
-#include "../main.h"
+#include "main.h"
 
 /**
  * main - check the code for Holberton School students.
@@ -8,7 +8,7 @@ int main(void)
 {
 	char *str;
 
-	str = "I do not fear computers. I fear the lack of them - Kareem Azeez";
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
 	print_rev(str);
 	return (0);
 }

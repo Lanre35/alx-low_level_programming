345b5467eceb9a1c4af38a3bbea06560a96e53c1
Lanre35 <lanreazeez99@yahoo.com>
2022-09-22 18:51:19 -0700

    done

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,25 +1,26 @@
 #include "main.h"
 
 /**
- * rot13 -  a function that encodes a string using rot13.
- * @s: An input string to encode using rot13
- * Return: An encode string
+ * rot13 - converts string to leet
+ * @c: character string pointer
+ * Return: char pointer
  */
-char *rot13(char *s)
+char *rot13(char *c)
 {
-	int i = 0;
+	int i, j;
 
-	while (s[i] != '\0')
+	char rot_13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm",
+	alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	for (i = 0; c[i] != '\0'; i++)
 	{
-		while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+		for (j = 0; j < 52; j++)
 		{
-			if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
-				s[i] += 13;
-			else
-				s[i] -= 13;
-			i++;
+			if (alphabet[j] == c[i])
+			{
+				c[i] = rot_13[j];
+				break;
+			}
 		}
-		i++;
 	}
-	return (s);
+	return (c);
 }

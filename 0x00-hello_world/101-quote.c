a4115c1c1de376d56b1128322bf253689f3f720b
Lanre35 <lanreazeez99@yahoo.com>
2022-10-12 14:18:08 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 
 /**
- * main - Entry point
- *
- * Descripion: using the main function
- * and that of place and that of art is useful" - Dora karpar,2019"
+ * main - main block
  * Return: 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora korper, 2015-10-2019\n", 59);
+	int s;
+
+	s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			s);
 	return (1);
 }
-

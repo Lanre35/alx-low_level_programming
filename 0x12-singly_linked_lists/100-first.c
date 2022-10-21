6267fd13996526bbdfb8ab5ab55a2ff2f93eaa8f
Lanre35 <lanreazeez99@yahoo.com>
2022-10-21 09:21:33 -0700

    done

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ * _startbeforemain - prints a senstence before the main is executed
+ * Return: 0
+ */
+void _startbeforemain(void) __attribute__ ((constructor));
+void _startbeforemain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
